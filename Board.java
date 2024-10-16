35c7ca46edf9441a291849d698818f9538cdb62f
mot8@txstate.edu <gentryrabe@gmail.com>
2024-10-15 20:06:26 -0500

    Updated board class initializing statment.

--- Board.java
+++ Board.java
@@ -16,7 +16,22 @@ public class Board{
         }
         return retStr;
     }
-    public Board(){
-        board = new Piece[8][8];
+    public Board() {
+        board = new Piece[8][8] = {
+            new Rook(new Location(7,0), "Black"), new Knight(new Location(7,1), "Black"), new Bishop(new Location(7,2), "Black"), new Queen(new Location(7,3), "Black"), new King(new Location(7,4), "Black"), new Bishop(new Location(7,5), "Black"), new Knight(new Location(7,6), "Black"), new Rook(new Location(7,7), "Black"),
+            null, null, null, null, null, null, null, null,
+            null, null, null, null, null, null, null, null,
+            null, null, null, null, null, null, null, null,
+            null, null, null, null, null, null, null, null,
+            null, null, null, null, null, null, null, null,
+            null, null, null, null, null, null, null, null,
+            new Rook(new Location(0,0), "White"), new Knight(new Location(0,1), "White"), new Bishop(new Location(0,2), "White"), new Queen(new Location(0,3), "White"), new King(new Location(0,4), "White"), new Bishop(new Location(0,5), "White"), new Knight(new Location(0,6), "White"), new Rook(new Location(0,7), "White")
+        }
+        for(int i = 0; i < 8; i++){
+            board[1][i] = new Pawn(new Location(6,i), "Black");
+        }
+        for(int i = 0 ; i < 8; i++){
+            board[6][i] = new Pawn(new Location(1,i), "White");
+        }
     }
 }
